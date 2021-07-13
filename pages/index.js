0b662de2253927ecdcba932a9a3870a2f9c79ae1
Bysl-soft-09 <debaray14@gmail.com>
2021-07-13 17:12:15 +0600

    link component navigation

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,16 @@
-function Home(){
-    return  <h1>Home Page</h1>
+import Link from 'next/link';
+
+function Home() {
+    return (<div>
+        <h1>Home Page</h1>
+        <Link href='/blog'>
+        <a>Blog</a>
+        </Link>
+        <Link href='/product'>
+        <a>Products</a>
+        </Link>
+
+    </div>)
 }
 
 export default Home;
