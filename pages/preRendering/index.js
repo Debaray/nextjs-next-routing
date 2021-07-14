4edeb8d302b91f0856d562f2031389b5d4b97ac1
Bysl-soft-09 <debaray14@gmail.com>
2021-07-14 12:48:03 +0600

    static generation

--- pages/preRendering/index.js
+++ pages/preRendering/index.js
@@ -1,6 +1,11 @@
+import Link from 'next/link';
 function Home(){
     return (
+        <>
         <h1>Next JS pre-rendering</h1>
+        <Link href='/preRendering/users'><a>Users</a></Link>
+        </>
+        
     )
 }
 export default Home;
